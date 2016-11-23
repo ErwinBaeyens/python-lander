983997b188d53ddbbef8e6b38c808caef7f0e538
Erwin Baeyens <erwin.baeyens@hgst.com>
2016-11-23 14:41:00 +0100

    addressed issues 1, 2, 3 and 5

--- lander.py
+++ lander.py
@@ -1,65 +1,69 @@
 #!/usr/bin/env python
 
-from string import lower
 import sys
 from time import sleep
 
 class Lander():
-	height = 0
-	velocity = 0
-	gravity = 1.622
-	fuel = 0
-	burn = 0
+    height = 0
+    velocity = 0
+    gravity = 1.622
+    fuel = 0
+    burn = 0
 
-	def __init__(self):
-		self.height = 1000
-		self.velocity = 50
-		self.fuel = 500
+    def __init__(self):
+        self.height = 1000
+        self.velocity = 50
+        self.fuel = 500
 
-	def print_reading(self):
-		if self.height == 1000:
-			print "Meter readings"
-			print "=============="
-			print "Fuel:{:4d} gal Height: {:6.3f} m speed {:6.3f} m/s".format(
-				self.fuel, self.height, self.velocity)
-		elif self.height > 0:
-			print "Fuel:{:4d} gal Height: {:6.3f} m speed {:6.3f} m/s".format(
-				self.fuel, self.height, self.velocity)
-		else:
-			print "Fuel:{:4d} gal Height: {:6.3f} m landingspeed {:6.3f} m/s".format(
-				self.fuel, self.height, self.velocity)
+    def print_reading(self):
+        if self.height == 1000:
+            print "Meter readings"
+            print "=============="
+            print "Fuel:{:4d} gal Height: {:6.3f} m speed {:6.3f} m/s".format(
+                self.fuel, self.height, self.velocity)
+        elif self.height > 0:
+            print "Fuel:{:4d} gal Height: {:6.3f} m speed {:6.3f} m/s".format(
+                self.fuel, self.height, self.velocity)
+        else:
+            print "Fuel:{:4d} gal Height: {:6.3f} m landingspeed {:6.3f} m/s".format(
+                self.fuel, self.height, self.velocity)
 
-	def update_parameters(self, burn, ):
-		pass
+    def update_parameters(self, burn, ):
+        pass
 
 # Lunar lander
 # rework of the original basic version
 
-print "You are aboard the lunar Lander and you are about to leave for the" 
-print "lunar surface"
-play = True
-while play:
-	print "You have left the spacecraft"
-	print "try to land with less than 5 m/sec velociy"
-	# rest of the program goes here
 
-	# temporary test code
-	lem = Lander()
-	lem.print_reading()
-	lem.height = 800
-	lem.print_reading()
-	lem.velocity=7
-	lem.height=0
-	lem.print_reading()
-	sleep(5)
-	
+def main():    
+    print "You are aboard the lunar Lander and you are about to leave for the" 
+    print "lunar surface"
+    play = True
+    while play:
+        print "You have left the spacecraft"
+        print "try to land with less than 5 m/sec velociy"
 
-	print "Do you want to play again (y/n)?"
-	y = lower(str(raw_input()))
+        # rest of the program goes here
+
+         # temporary test code
+        lem = Lander()
+        lem.print_reading()
+        lem.height = 800
+        lem.print_reading()
+        lem.velocity=7
+        lem.height=0
+        lem.print_reading()
+        sleep(5)
+
+        print "Do you want to play again (y/n)?"
+        y = str(raw_input()).lower()
 	if y == 'n' or y == "no":
-		play = False
-		del lem
-		
+            play = False
+	    del lem
 
-print "Have a nice day"
-sys.exit(0)
+        print "Have a nice day"
+        sys.exit(0)
+
+    
+if __name__ == "__main__":
+    main()
